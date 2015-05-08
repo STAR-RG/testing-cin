e34887d2bb47abf5e934c1669881b963c3c9755f
Marcelo d'Amorim <damorim@cin.ufpe.br>
2015-05-08 19:24:17 -0300

    adding tests for doublelinkedlist

--- doublelinkedlist/src/test/java/DoubleLinkedListTest.java
+++ doublelinkedlist/src/test/java/DoubleLinkedListTest.java
@@ -6,10 +6,42 @@ import java.util.NoSuchElementException;
 public class DoubleLinkedListTest {
 
     @Test(expected=NoSuchElementException.class)
-    public void testCycle() {
+    public void testFirstAtEmptyList() {
         DoubleLinkedList dll = new DoubleLinkedList();
         dll.getFirst();
     }
+
+
+    @Test
+    public void testAddOne() {
+        DoubleLinkedList dll = new DoubleLinkedList();
+	dll.add("one element");
+        dll.getFirst();
+    }
+
+    @Test
+    public void testAddAll() {
+        DoubleLinkedList dll = new DoubleLinkedList();
+	java.util.Collection col = java.util.Arrays.asList("one element");
+	dll.addAll(0, col);
+    }
+
+    @Test
+    public void testAddAllWithEmptyCollection() {
+        DoubleLinkedList dll = new DoubleLinkedList();
+	java.util.Collection col = new java.util.ArrayList();
+	dll.addAll(0, col);
+    }
+
+    @Test
+    public void testAddAllAtEnd() {
+        DoubleLinkedList dll = new DoubleLinkedList();
+	dll.add("one element");
+	java.util.Collection col = java.util.Arrays.asList("another element");
+	dll.addAll(0, col);
+    }
+
+
 
 
 }
