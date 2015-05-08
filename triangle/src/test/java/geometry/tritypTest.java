287a5dda25e15233cb00ac3e8d46f7f4a8d2de08
Marcelo d'Amorim <damorim@cin.ufpe.br>
2015-05-08 19:24:34 -0300

    adding tests for triangle

--- triangle/src/test/java/geometry/tritypTest.java
+++ triangle/src/test/java/geometry/tritypTest.java
@@ -6,18 +6,44 @@ import org.junit.Assert;
 public class tritypTest {
 
     @Test
-    public void testEqualSizes() {
+    public void testEqualateral() {
         Assert.assertEquals(trityp.type(1,1,1), 3);
     }
 
     @Test
-    public void testZeros() {
+    public void testIsosceles() {
+        Assert.assertEquals(trityp.type(4,5,4), 2);
+    }
+
+    @Test
+    public void testNotTriangle1() {
+        Assert.assertEquals(trityp.type(2,1,3), 4);
+    }
+
+    @Test
+    public void testNotTriangle2() {
         Assert.assertEquals(trityp.type(0,0,0), 4);
     }
 
     @Test
-    public void testImpossible() {
+    public void testNotTriangle3() {
         Assert.assertEquals(trityp.type(1,1,3), 4);
     }
+
+    @Test
+    public void testNotTriangle4() {
+        Assert.assertEquals(trityp.type(0,1,1), 4);
+    }
+
+    @Test
+    public void testNotTriangle5() {
+        Assert.assertEquals(trityp.type(1,0,1), 4);
+    }
+
+    @Test
+    public void testNotTriangle6() {
+        Assert.assertEquals(trityp.type(1,1,0), 4);
+    }
+
 
 }
