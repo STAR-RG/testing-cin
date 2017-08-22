03a8794081fecd796a2d59a003177316d11e0aab
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2017-08-22 05:23:38 -0300

    updating demos

--- demos/basics/median/src/test/java/MedianTest.java
+++ demos/basics/median/src/test/java/MedianTest.java
@@ -4,19 +4,29 @@ import static numeric.Median.median;
 
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Ignore;
 
 public class MedianTest {
 
-    @Test public void midIsFirst() {
+    @Test
+    public void midIsFirst() {
         Assert.assertEquals(2, median(2, 1, 3));
     }
 
-    @Test public void midIsLast() {
+    @Test
+    public void midIsLast() {
         Assert.assertEquals(2, median(1, 3, 2));
     }
     
-    @Test public void midIsMiddle() {
+    @Test
+    public void midIsMiddle() {
         Assert.assertEquals(2, median(1, 2, 3));
+    }
+
+    @Ignore
+    @Test
+    public void infiniteLoop() {
+        while (true) {}
     }
 
 }
--- demos/basics/median/src/test/java/MoreMedianTest.java
+++ demos/basics/median/src/test/java/MoreMedianTest.java
@@ -7,23 +7,28 @@ import org.junit.Assert;
 
 public class MoreMedianTest {
 
-    @Test public void midIsFirst() {
+    @Test
+    public void midIsFirst() {
        Assert.assertEquals(-2, median(-2, -1, -3));
     }
 
-    @Test public void midIsSecond() {
+    @Test
+    public void midIsSecond() {
        Assert.assertEquals(-2, median(-1, -2, -3));
     }
 
-    @Test public void midIsLast() {
+    @Test
+    public void midIsLast() {
        Assert.assertEquals(-2, median(-1, -3, -2));
     }
 
-    @Test public void midIsSameLo() {
+    @Test
+    public void midIsSameLo() {
        Assert.assertEquals(0, median(0, 1, 0));
     }
 
-    @Test public void midIsSameHi() {
+    @Test
+    public void midIsSameHi() {
        Assert.assertEquals(2, median(2, 1, 2));
     }
 
