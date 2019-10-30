1d160f5d708867648803a2379467bdfe1b60e03c
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-10-30 18:37:35 -0300

    adicionando gabarito + pequenas mudancas

---
+++ demos/README.md
@@ -0,0 +1,64 @@
+# testing-cin
+
+List of tools we see in this course, grouped by categories.  For
+example, you will find under directory "median" an example on how to
+use the gradle build system.<br>
+
+------------
+Basic
+------------
+
+<p><b>Build Systems</b><br>
+&nbsp;&nbsp;&nbsp;ant: demo/basics/texasholdem<br>
+&nbsp;&nbsp;&nbsp;gradle: demo/basics/median<br>
+</p>
+
+<p><b>Test Execution</b><br>
+&nbsp;&nbsp;&nbsp;junit4: demo/basics/median<br>
+</p>
+
+<p><b>Fluent Assertions</b><br>
+&nbsp;&nbsp;&nbsp;hamcrest: demo/basics/binarysearch<br>
+</p>
+
+<p><b>Test Suite Quality</b><br>
+<!-- indent this please -->
+<p>Coverage<br>
+   &nbsp;&nbsp;&nbsp;jacoco: demo/basics/doublelinkedlist<br>
+</p>
+<p>Mutation<br>
+&nbsp;&nbsp;&nbsp;pit: demo/basics/triangle<br>
+</p>
+
+</p>
+
+<p><b>Functional Testing</b><br>
+&nbsp;&nbsp;&nbsp;pairwise: demo/basics/pairwise<br>
+</p>
+
+------------
+Advanced
+------------
+
+<p><b>GUI Testing</b><br>
+&nbsp;&nbsp;&nbsp;assertj+swing: demo/advanced/assertj+swing<br>
+&nbsp;&nbsp;&nbsp;selenium: demo/advanced/selenium<br>
+&nbsp;&nbsp;&nbsp;android-espresso: demo/advanced/android-espresso<br>
+</p>
+
+<p><b>Load Testing</b><br>
+&nbsp;&nbsp;&nbsp;JMeter: demo/advanced/jmeter<br>
+</p>
+
+<p><b>DevOps</b><br>
+&nbsp;&nbsp;&nbsp;Docker: demo/advanced/docker<br>
+&nbsp;&nbsp;&nbsp;Vagrant: demo/advanced/vagrant-tutorial<br>
+</p>
+
+<p><b>JavaScript</b><br>
+&nbsp;&nbsp;&nbsp;QUnit: demo/advanced/qunit<br>
+</p>
+
+
+Enjoy,<br>
+Marcelo
--- demos/advanced/selenium/DemoSeleniumJava/DemoSimples/src/test/java/TesteSimples.java
+++ demos/advanced/selenium/DemoSeleniumJava/DemoSimples/src/test/java/TesteSimples.java
@@ -16,100 +16,38 @@ import org.openqa.selenium.support.ui.Select;
 public class TesteSimples {
 
   private WebDriver driver;
-  private String strUrlLogin = "https://enterprise-demo.orangehrmlive.com/auth/login";
+  private String strUrlLogin = "https://www.amazon.com/";
 
   @Before
   public void setUp() throws Exception {
-    this.driver = new HtmlUnitDriver();
-    //this.driver = new FirefoxDriver();
-    this.driver.manage().window().maximize();
+    driver = new HtmlUnitDriver();
+    //driver = new FirefoxDriver();
+    driver.manage().window().maximize();
   }
 
   @Test
   public void testLoginSucesso() {
-    this.driver.get(this.strUrlLogin);
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    driver.get(this.strUrlLogin);
 
-    WebElement login = this.driver.findElement(By.id("txtUsername"));
-    login.clear();
-    login.sendKeys("linda.anderson");
+    WebElement deals = driver.findElement(By.id("Today's deals"));
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Deals and Promotion")));
 
-    WebElement senha = this.driver.findElement(By.name("txtPassword"));
-    senha.clear();
-    senha.sendKeys("linda.anderson");
+    WebElement books = driver.findElement(By.name("Books"));
+    books.click();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results for Books")));    
+
+    WebElement addtocart = driver.findElement(By.name("Add to Cart"));
+    addtocart.click();
+
+    WebElement cart = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div/div[2]/div[2]/div/a[4]/span[1]"));
+    cart.click();
     
-    WebElement submit = this.driver.findElement(By.id("btnLogin"));
-    submit.click();
-
-    WebDriverWait wait = new WebDriverWait(this.driver, 10);
-
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("welcome")));
-    boolean existeTxtBemvindo = this.driver.getPageSource().contains("Welcome");
-    Assert.assertTrue(existeTxtBemvindo);
-
-  }
-
-  @Test
-  public void testLoginErro() {
-    this.driver.get(this.strUrlLogin);
-
-    WebElement login = this.driver.findElement(By.name("txtUsername"));
-    login.clear();
-    login.sendKeys("sysadmin");
-
-    WebElement senha = this.driver.findElement(By.name("txtPassword"));
-    senha.clear();
-    senha.sendKeys("");
-    
-    WebElement submit = this.driver.findElement(By.id("btnLogin"));
-    submit.click();
-
-    this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    boolean existeMsgErro = this.driver.getPageSource().contains("Password cannot be empty");
-    Assert.assertTrue(existeMsgErro);
-  }
-
-
-  @Test
-  public void testAtualizarDadosPessoais() {
-    this.driver.get(this.strUrlLogin);
-
-    WebElement login = this.driver.findElement(By.name("txtUsername"));
-    login.clear();
-    login.sendKeys("linda.anderson");
-
-    WebElement senha = this.driver.findElement(By.name("txtPassword"));
-    senha.clear();
-    senha.sendKeys("linda.anderson");
-    
-    WebElement submit = this.driver.findElement(By.id("btnLogin"));
-    submit.click();
-
-    //Navegar para a tela de atualizacao
-    WebElement elemento = this.driver.findElement(By.id("menu_pim_viewMyDetails"));
-    elemento.click();
-
-    elemento = this.driver.findElement(By.id("btnSave"));
-    elemento.click();
-
-    elemento = this.driver.findElement(By.id("personal_txtEmpFirstName"));
-    elemento.clear();
-    elemento.sendKeys("Joana");
-
-    Select select = new Select(driver.findElement(By.id("personal_cmbMarital")));
-                  
-    select.selectByValue("Single");
-
-    elemento = this.driver.findElement(By.id("btnSave"));
-    elemento.click();
-
-    this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    boolean existeMsgSucesso = this.driver.getPageSource().contains("Successfully Saved");
-    Assert.assertTrue(existeMsgSucesso);
   }
 
 
   @After
   public void tearDown() throws Exception {
-    this.driver.quit();
+    driver.quit();
   }
 }
